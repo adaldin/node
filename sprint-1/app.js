8d476dfccce7baf91c1e29233074f2fb850bdfe3
Hiena <aledaldin@gmail.com>
2022-10-03 08:37:11 +0200

    module path and os

--- sprint-1/app.js
+++ sprint-1/app.js
@@ -1,3 +1,7 @@
 const path = require("node:path");
 var object = path.parse(__filename);
 console.log(object);
+
+const os = require("node:os");
+var totalMemory = os.totalmem();
+console.log(`Total memory: ${totalMemory}`);
